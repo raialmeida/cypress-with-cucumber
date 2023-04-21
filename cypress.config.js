ada8ad341c16761f5337882d243a0e15b6e6c94b
Rai Andrade <rai.andrade.ext@dasa.com.br>
2023-04-21 15:17:11 -0300

    ajustes eslint

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,25 @@
 const { defineConfig } = require("cypress")
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor")
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify")
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 async function setupNodeEvents(on, config) {
-  await preprocessor.addCucumberPreprocessorPlugin(on, config)
-  on("file:preprocessor", browserify.default(config))
-  allureWriter(on, config)
-  return config
+    await preprocessor.addCucumberPreprocessorPlugin(on, config)
+    on("file:preprocessor", browserify.default(config))
+    allureWriter(on, config)
+    return config
 }
 
 module.exports = defineConfig({
-  e2e: {
-    env: {
-      omitFiltered: true,
-      filterSpecs: true
+    e2e: {
+        env: {
+            omitFiltered: true,
+            filterSpecs: true
+        },
+        baseUrl: "https://bugbank.netlify.app/",
+        specPattern: "**/*.feature",
+        video: false,
+        fixturesFolder: false,
+        setupNodeEvents,
     },
-    baseUrl: "https://bugbank.netlify.app/",
-    specPattern: "**/*.feature",
-    video: false,
-    fixturesFolder: false,
-    setupNodeEvents,
-  },
 })

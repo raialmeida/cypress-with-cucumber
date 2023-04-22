64f739f55b66d81e0336f5d3b240f34a1a82165d
Rai Andrade <rai.andrade.ext@dasa.com.br>
2023-04-22 15:17:58 -0300

    ajustes arquivo step

--- cypress/e2e/steps/cadastroUsuario.js
+++ cypress/e2e/steps/cadastroUsuario.js
@@ -2,15 +2,15 @@ const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor")
 import { faker } from '@faker-js/faker'
 import { cadastroUsuario } from "../pages/cadastroUsuario"
 
-Given(/^que esteja na pagina de cadastro de usuario$/, () => {
+Given(/^que esteja na página de cadastro de usuário$/, () => {
     cadastroUsuario.btnRegistrar()
 })
 
-When(/^preenho os informção de cadastro$/, () => {
+When(/^preencho as informações de cadastro$/, () => {
     cadastroUsuario.preencherFormulario(faker.internet.email(), faker.name.fullName(), '123456', '123456')
     cadastroUsuario.btnCadastrar()
 })
 
-Then(/^deve exibir a msg de usuario cadastrada$/, () => {
+Then(/^deve exibir a mensagem de usuário cadastrado$/, () => {
     cadastroUsuario.validarModalText('foi criada com sucesso')
 })
